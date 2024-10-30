ec1eff47dd8e7697ceb07254c900aa79240c86c6
B200896 <kashishkaramchandani3@gmail.com>
2024-10-30 11:22:16 +0530

    code updated final

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+import MainPage from './components/Mainpage';
+
+const Home = () => {
+  return (
+    <div>
+     <MainPage/>
+    </div>
+  );
+};
+
+export default Home;
